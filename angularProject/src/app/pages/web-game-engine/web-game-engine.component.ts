44371c0164c85ffc3b12680ca5a8b340f91ce303
Alexandre Brière <alexandre-1.briere@polymtl.ca>
2020-05-30 17:31:09 -0400

    finishing the init
    the init method in the windowCreatorService now properly append a canvas of user defined size to a div passed in parameter of the function

--- angularProject/src/app/pages/web-game-engine/web-game-engine.component.ts
+++ angularProject/src/app/pages/web-game-engine/web-game-engine.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {WindowCreatorService} from "../../services/webGameEngine/windowCreator/window-creator.service";
 
 @Component({
   selector: 'app-web-game-engine',
   templateUrl: './web-game-engine.component.html',
   styleUrls: ['./web-game-engine.component.scss']
 })
-export class WebGameEngineComponent implements OnInit {
+export class WebGameEngineComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('div') div: HTMLDivElement;
+  constructor(private windowCreator: WindowCreatorService) { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+    this.windowCreator.init(this.div, 500, 500);
   }
 
 }
--- angularProject/src/app/services/webGameEngine/windowCreator/window-creator.service.ts
+++ angularProject/src/app/services/webGameEngine/windowCreator/window-creator.service.ts
@@ -1,20 +1,30 @@
-import { Injectable } from '@angular/core';
+import {ElementRef, Injectable, Renderer2} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WindowCreatorService {
   container: HTMLDivElement | undefined;
+  window: HTMLCanvasElement | undefined;
   dimension: number[];
 
   constructor() {
     this.container = undefined;
+    this.window = undefined;
     this.dimension = [];
   }
 
-  init(container: HTMLDivElement) {
-    this.container = container;
-    this.dimension[0] = container.offsetHeight;
-    this.dimension[1] = container.offsetWidth;
+  init(container: HTMLDivElement, width: number, height: number) {
+    if(container) {
+      this.container = container;
+      this.dimension[0] = height;
+      this.dimension[1] = width;
+      this.window = document.createElement('canvas');
+      this.window.setAttribute('height', this.dimension[0].toString());
+      this.window.setAttribute('width', this.dimension[1].toString());
+      console.log(this.container);
+      console.log(this.window);
+      (this.container as unknown as ElementRef<HTMLDivElement>).nativeElement.appendChild(this.window);
+    }
   }
 }
